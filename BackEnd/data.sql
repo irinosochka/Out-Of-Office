223b083f36dd1333a20c2f2c7d7cd366ae2f872e
irinosochka <irinovoselska@gmail.com>
2024-07-06 03:40:27 +0200

    added data for creating table

---
+++ BackEnd/data.sql
@@ -0,0 +1,47 @@
+CREATE DATABASE out_of_office;
+
+USE out_of_office;
+
+CREATE TABLE Employees (
+    EmployeeID INT AUTO_INCREMENT PRIMARY KEY,
+    FullName VARCHAR(255) NOT NULL,
+    Subdivision VARCHAR(255) NOT NULL,
+    Position VARCHAR(255) NOT NULL,
+    Status ENUM('Active', 'Inactive') NOT NULL,
+    PeoplePartner INT,
+    OutOfOfficeBalance INT NOT NULL,
+    Photo BLOB,
+    FOREIGN KEY (PeoplePartner) REFERENCES Employees(EmployeeID)
+);
+
+CREATE TABLE LeaveRequests (
+    LeaveRequestID INT AUTO_INCREMENT PRIMARY KEY,
+    EmployeeID INT NOT NULL,
+    AbsenceReason VARCHAR(255) NOT NULL,
+    StartDate DATE NOT NULL,
+    EndDate DATE NOT NULL,
+    Comment TEXT,
+    Status ENUM('New', 'Submitted', 'Approved', 'Rejected', 'Canceled') NOT NULL,
+    FOREIGN KEY (EmployeeID) REFERENCES Employees(EmployeeID)
+);
+
+CREATE TABLE ApprovalRequests (
+    ID INT AUTO_INCREMENT PRIMARY KEY,
+    Approver INT,
+    LeaveRequestID INT,
+    Status ENUM('New', 'Approved', 'Rejected') NOT NULL,
+    Comment TEXT,
+    FOREIGN KEY (Approver) REFERENCES Employees(EmployeeID),
+    FOREIGN KEY (LeaveRequestID) REFERENCES LeaveRequests(LeaveRequestID)
+);
+
+CREATE TABLE Projects (
+    ID INT AUTO_INCREMENT PRIMARY KEY,
+    ProjectType VARCHAR(255) NOT NULL,
+    StartDate DATE NOT NULL,
+    EndDate DATE,
+    ProjectManager INT,
+    Comment TEXT,
+    Status ENUM('Active', 'Inactive') NOT NULL,
+    FOREIGN KEY (ProjectManager) REFERENCES Employees(EmployeeID)
+);
--- BackEnd/server.js
+++ BackEnd/server.js
@@ -5,8 +5,14 @@ const cors = require('cors');
 const fs = require('fs');
 const path = require("path");
 
+const PORT = process.env.PORT || 8082
+
 const app = express();
-app.use(cors());
+
+app.use(cors({
+    credentials: true,
+    origin: 'http://localhost:3000'
+}))
 
 // Database connection configuration
 const db = mysql.createConnection({
@@ -20,39 +26,48 @@ const db = mysql.createConnection({
 const sqlFilePath = path.join(__dirname, 'data.sql');
 
 // Connect to MySQL database
-db.connect(function(err) {
-    if (err) throw err;
-    console.log("Connected to MySQL!");
 
-    // Check if the database exists
-    db.query("SHOW DATABASES LIKE 'out_of_office';", function(err, result) {
+const dbCheck = async () => {
+    db.connect(function(err) {
         if (err) throw err;
-        if (result.length === 0) {
-            // Database does not exist
-            fs.readFile(sqlFilePath, 'utf8', (err, sqlScript) => {
-                if (err) {
-                    console.error("Error reading SQL file:", err);
-                    return;
-                }
+        console.log("Connected to MySQL!");
 
-                db.query(sqlScript, function (err, result) {
-                    if (err) throw err;
-                    console.log("Database and tables created");
+        // Check if the database exists
+        db.query("SHOW DATABASES LIKE 'out_of_office';", function(err, result) {
+            if (err) throw err;
+            if (result.length === 0) {
+                // Database does not exist
+                fs.readFile(sqlFilePath, 'utf8', (err, sqlScript) => {
+                    if (err) {
+                        console.error("Error reading SQL file:", err);
+                        return;
+                    }
+
+                    db.query(sqlScript, function (err, result) {
+                        if (err) throw err;
+                        console.log("Database and tables created");
+                    });
                 });
-            });
-        } else {
-            // Database already exist
-            console.log("Database 'out_of_office' already exists");
-        }
+            } else {
+                // Database already exist
+                console.log("Database 'out_of_office' already exists");
+            }
+        });
     });
-});
+}
 
-// Testing server connection
+const start = async () => {
+    try {
+        await dbCheck();
+        app.listen(PORT, () => console.log('Server is running on ${PORT}'))
+    } catch (e) {
+        console.log(e)
+    }
+}
+
+start().then(r => r)
+
+//TEST
 app.get('/', (req, res) => {
     return res.json("Test");
-});
-
-// Start server on port 8081
-app.listen(8081, () => {
-    console.log("Server is running on http://localhost:8081");
 });
